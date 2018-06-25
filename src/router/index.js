fcdec3a33f31ff2f5d1eb07931648c55e5cf6a13
江湖小虾米 <366797700@qq.com>
2018-06-25 22:55:56 +0800

    分支第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 // 导入组件
 import seller from '@/components/seller/seller.vue'
-import goods from '@/components/goods/goods.vue'
+// import goods from '@/components/goods/goods.vue'
 // import ratings from '@/components/ratings/ratings.vue'
 
 Vue.use(Router)
@@ -12,7 +12,7 @@ export default new Router({
   routes: [
     { path:'/',redirect:'/goods'},
     { path:'/seller',component:seller },
-    { path:'/goods',component:goods },
+    { path:'/goods',component: resolve => require(['@/components/goods/goods.vue'],resolve) },
     { path:'/ratings',component: resolve => require(['@/components/ratings/ratings.vue'],resolve) },
   ],
   linkActiveClass:"active"
